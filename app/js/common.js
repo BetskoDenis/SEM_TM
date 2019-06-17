6e58f76ddb1db581a2230adbfb4811c337f45f98
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2019-06-17 19:00:10 +0300

    v004

--- app/js/common.js
+++ app/js/common.js
@@ -7,4 +7,43 @@ $(document).ready(function () {
         smartSpeed: 1300
 
     });
+$('.button_burger').on('click', function () {
+    var burgerButton = $('.button_burger span:first-child'),
+        burgerSpan = $('.button_burger span');
+        $('.button_burger').toggleClass('click')
+    if($('.button_burger').hasClass('click')){
+        $('.menu_right_block.mobile').css('right','0')
+        $('.button_burger').css('background-color','rgba(22,28,36,0)')
+        burgerSpan.css('transform', 'translateX(-200px)')
+        //$('.button_burger span:nth-child(4)').css('transform', 'translateX(-50px)')
+        //$('.button_burger span:nth-child(5)').css('transform', 'translateX(-50px)')
+        $('.button_burger span:nth-child(4)').css('transform', 'rotate(45deg)')
+        $('.button_burger span:nth-child(5)').css('transform', 'rotate(-45deg)')
+    }else{
+        burgerSpan.css('transform', 'translateX(0)')
+        $('.menu_right_block.mobile').css('right','-500px')
+        $('.button_burger').css('background-color','rgba(22,28,36,1)')
+    }
+    })
+    $('.certificate_block').owlCarousel({
+        items: 4,
+        loop: true,
+        dots: false,
+        nav: true,
+        smartSpeed: 1300,
+        responsive:{
+            0:{
+                items:1
+            },
+            450:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
 })
